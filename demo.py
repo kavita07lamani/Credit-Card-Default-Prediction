c06c17066db23b699dd58a88f021d8fbefc0d68f
kavita07lamani <kavitamlamani@gmail.com>
2024-04-25 14:17:57 +0530

    data ingestion

---
+++ demo.py
@@ -0,0 +1,5 @@
+from credit_card.pipline.training_pipeline import TrainPipeline
+
+
+pipline  = TrainPipeline()
+pipline.run_pipeline()
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="credit",
+    version="0.0.0",
+    author="kavita",
+    author_email="kavitamlamani@gmail.com",
+    packages=find_packages()
+)
